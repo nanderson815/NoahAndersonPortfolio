e874e2fa791450d365bfe572b09d07209d36cf8e
Noah A <nanderson815@gmail.com>
2019-01-21 16:42:08 -0500

    added a better header image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
 
     $('.parallax').parallax();
 
+    $(window).scroll(() => {
+        if (scrollY > 150) {
+            $('#navBar').addClass('blue darken-4');
+
+        } else {
+            $('#navBar').removeClass('blue darken-4');
+
+        }
+    })
+
 });
