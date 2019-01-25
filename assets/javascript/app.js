aca1283ae227883a276abe69f43c92e73e3b937c
Noah A <nanderson815@gmail.com>
2019-01-24 19:31:31 -0500

    added more animations"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     $('.parallax').parallax();
 
-
+  
     $(window).scroll(() => {
         console.log(scrollY);
         if (scrollY > 50) {
@@ -17,6 +17,13 @@ $(document).ready(function () {
 
         }
 
+        if (scrollY > 90) {
+            $('#aboutMeHeader, #aboutMeP').removeClass('hidden');
+            $('#aboutMeHeader').addClass('slideRight');
+            $('#aboutMeP').addClass('slideUp');
+
+
+        }
     })
 
     // $('#headerName').on('click', () => {
